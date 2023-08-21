b7b194fad37d16ada8884ec1f855827f688a220a
Dan Web <128343993+therealdanweb@users.noreply.github.com>
2023-08-21 11:10:14 +0300

    Update produs_cifre_impare.c

--- produs_cifre_impare.c
+++ produs_cifre_impare.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main()
+{
+	char c, ok = 0; unsigned p = 1;
+	
+	while (scanf("%c", &c) != EOF && c >= '0' && c <= '9') {
+		if ((c - '0') % 2) {
+			p *= (c - '0');
+			ok = 1;
+		}
+	}
+	
+	ok ? printf("%u", p) : printf("-1");
+
+	return 0;
+}
